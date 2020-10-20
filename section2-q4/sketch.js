8c7b1cb75e05b028f71e0961115614c072880541
MatsuedaHayato <1900888h@stu.kobe-u.ac.jp>
2020-10-21 01:57:18 +0900

    Change section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,10 @@
-// ギリシャ国旗
+// バングラデシュ国旗
 function setup() {
-  const blue = color(0, 51, 160);
   createCanvas(270, 180);
   // noStroke();
   background(255);
-
-  let d = height / 9; // 縞1本の太さ
-
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
-
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
-
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  fill(15, 64, 15);
+  rect(0, 0, 270, 180);
+  fill(255, 0, 0);
+  ellipse(100, 90, 110);
 }
