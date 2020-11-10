74afb7899302df8f9f59c1b95e931eae3e437897
MatsuedaHayato <1900888h@stu.kobe-u.ac.jp>
2020-11-10 13:28:51 +0900

    change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,6 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
@@ -11,6 +12,11 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  if(count < 50){
+    size = count
+  }
+  else{
+    size = -1 * count + 100
+  }
   ellipse(width / 2, height / 2, size);
 }
