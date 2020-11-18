242d7a6c839dd0bc96355eee996ad892729ed283
MatsuedaHayato <1900888h@stu.kobe-u.ac.jp>
2020-11-18 11:39:57 +0900

    change section2-q4 (2)

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,10 +1,18 @@
-// バングラデシュ国旗
+// タイ国旗
 function setup() {
   createCanvas(270, 180);
-  // noStroke();
+   noStroke();
   background(255);
-  fill(15, 64, 15);
-  rect(0, 0, 270, 180);
-  fill(255, 0, 0);
-  ellipse(100, 90, 110);
+  for(i = 0; i < 6; i++){
+    if(i == 2 || i == 3){
+      fill(22, 2, 156);
+    }
+    else if(i == 1 || i == 4){
+      fill(255);
+    }
+    else{
+      fill(235, 52, 52);
+    }
+    rect(0, 30 * i, 270, 30);
+  }
 }
