6ec9a98af9a83b884f222b0cabad0e6ac8a577d6
MatsuedaHayato <1900888h@stu.kobe-u.ac.jp>
2020-11-22 22:35:54 +0900

    change section2-q3 (2)

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -16,10 +16,14 @@ function setup() {
   maxR = min(width, height);
 
   drawCircle(black, maxR);
-  drawArcs(green, red, maxR * 0.8);
-  drawArcs(cream, black, maxR * 0.75);
-  drawArcs(green, red, maxR * 0.5);
-  drawArcs(cream, black, maxR * 0.45);
+  for(let i = 1; i < 5; i++){
+    if(i % 2 == 1){
+      drawArcs(green, red, maxR * (-0.15 * i + 0.95));
+    }
+    else{
+      drawArcs(cream, black, maxR * (-0.15 * i + 1.05));
+    }
+  }
   drawCircle(green, maxR * 0.1);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
   drawCircle(red, maxR * 0.05);
